dde267ed9880b6b584a6bff48c847020fd514bbb
temmem chouchen <temmemchouchen@gmail.com>
2023-06-24 16:06:49 +0100

    ti2

---
+++ js/js.js
@@ -0,0 +1,28 @@
+var button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+    alert('we will clean your device and return like new');
+});
+var button = document.getElementById('myButton1');
+button.addEventListener('click', function() {
+    alert('we will do a total diagnostic to know all the problems with your device');
+});
+var button = document.getElementById('myButton2');
+button.addEventListener('click', function() {
+    alert('we will test everything to know that there are no problems with yuor device');
+});
+var button = document.getElementById('myButton3');
+button.addEventListener('click', function() {
+    alert('we will do our best to fix all the problems of your device');
+});
+var button = document.getElementById('myButton4');
+button.addEventListener('click', function() {
+    alert('we will fix most of problems with your prnter/photocopier');
+});
+var button = document.getElementById('myButton5');
+button.addEventListener('click', function() {
+    alert('we will look for every solution to make the device work as new');
+});
+var button = document.getElementById('myButtonC');
+button.addEventListener('click', function() {
+    alert('thnk you for contacting us');
+});
