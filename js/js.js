bf28e489a0cad02b19e3c7d1bdb98a9bf0a8e896
temmem chouchen <temmemchouchen@gmail.com>
2023-06-25 17:54:28 +0100

    ti3

--- js/js.js
+++ js/js.js
@@ -1,28 +1,28 @@
-var button = document.getElementById('myButton');
-button.addEventListener('click', function() {
-    alert('we will clean your device and return like new');
+console.log("hello");
+
+
+var button1 = document.getElementById('myButton');
+button1.addEventListener('click', function () {
+    alert('we will clean your device and return it like new');
 });
-var button = document.getElementById('myButton1');
-button.addEventListener('click', function() {
+var button2 = document.getElementById('myButton1');
+button2.addEventListener('click', function () {
     alert('we will do a total diagnostic to know all the problems with your device');
 });
-var button = document.getElementById('myButton2');
-button.addEventListener('click', function() {
-    alert('we will test everything to know that there are no problems with yuor device');
+var button3 = document.getElementById('myButton2');
+button3.addEventListener('click', function () {
+    alert('we will test everything to know that there are no problems with your device');
 });
-var button = document.getElementById('myButton3');
-button.addEventListener('click', function() {
+var button4 = document.getElementById('myButton3');
+button4.addEventListener('click', function () {
     alert('we will do our best to fix all the problems of your device');
 });
-var button = document.getElementById('myButton4');
-button.addEventListener('click', function() {
+var button5 = document.getElementById('myButton4');
+button5.addEventListener('click', function () {
     alert('we will fix most of problems with your prnter/photocopier');
 });
-var button = document.getElementById('myButton5');
-button.addEventListener('click', function() {
+var button6 = document.getElementById('myButton5');
+button6.addEventListener('click', function () {
     alert('we will look for every solution to make the device work as new');
 });
-var button = document.getElementById('myButtonC');
-button.addEventListener('click', function() {
-    alert('thnk you for contacting us');
-});
+
---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("hello");
+
+var button7 = document.getElementById('myButton6');
+button7.addEventListener('click', function () {
+    alert('thnk you for contacting us');
+});
